1ff91d1483a9deff8dc08a4cdb2f994465c639a9
105074314 <105074314@student.swin.edu.au>
2025-05-25 00:42:26 +1000

    Update settings.php

--- settings.php
+++ settings.php
@@ -3,9 +3,14 @@ host, user, password and database name connection variables, and use this in you
 connect to your MySQL database on the local database server.
 
 <?php
-// Database connection settings
 $host = "localhost";
-$user = "root";
-$password = "";
-$database = "bluewave_db";
+$username = "root"; 
+$password = ""; 
+$database = "exhibition_db";
+
+$conn = mysqli_connect(hostname: $host, username: $username, password: $password, database: $database);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
